b53bf42ebf1bc646ddda74054e2c29a20f5e2502
A Piranha <piranha9191@gmail.com>
2025-10-21 21:35:16 -0700

    Version bump

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -12,7 +12,7 @@ namespace MO2ExportImport.ViewModels
 {
     public class MainViewModel : ReactiveObject
     {
-        public string ProgramVersion { get; } = "1.5";
+        public string ProgramVersion { get; } = "1.6";
 
         private const string SettingsFilePath = "settings.json";
 
